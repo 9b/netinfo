e36e74525be7dbee983259a4678a85b8babd29e4
9b <brandon@9bplus.com>
2018-12-25 16:24:57 -0500

    Updated the API to include more calls and additional helper logic.

--- README.rst
+++ README.rst
@@ -30,12 +30,33 @@ Sample Output
 -------------
 When calling http://localhost:7777/lookup?ip=8.8.8.8::
 
-    {
-      "as_name": "GOOGLE - Google LLC, US",
-      "as_num": 15169,
-      "ip": "8.8.8.8",
-      "netblock": "8.8.8.0/24"
-    }
+  {
+    "as_name": "GOOGLE - Google LLC, US",
+    "as_num": 15169,
+    "ip": "8.8.8.8",
+    "ip_hex": "0x8080808",
+    "ip_version": 4,
+    "network": "8.8.8.0/24",
+    "network_broadcast": "8.8.8.255",
+    "network_hostmask": "0.0.0.255",
+    "network_netmask": "255.255.255.0",
+    "network_size": 256
+  }
 
-Unlike the standard PyASN library, NetInfo will add the AS name to the response and original IP address that was requested.
+Unlike the standard PyASN library, NetInfo will add the AS name to the response, additional network data and the original IP address that was requested.
 
+API Endpoints
+-------------
+The following endpoints are available within the NetInfo service.
+
+**/lookup?ip=8.8.8.8**
+Get back AS and network information for an IP address.
+
+**/network-addresses?cidr=8.8.8.0/24**
+Get back all IP addresses as part of a network range.
+
+**/prefixes?asn=15169**
+Get back all prefixes advertised for a specific AS network.
+
+**/as-name?asn=15169**
+Get back the name of the AS network.
--- app/core/api.py
+++ app/core/api.py
@@ -6,6 +6,7 @@ from . import core
 from .. import mongo, logger, celery, check_asndb
 from flask import (jsonify, request)
 from flask import current_app as app
+from netaddr import IPAddress, IPNetwork
 
 
 @core.route('/lookup', methods=['GET'])
@@ -14,9 +15,56 @@ def lookup():
     """Enrich IP address."""
     ip_addr = request.args.get('ip')
     data = app.config['ASNDB'].lookup(ip_addr)
-    record = {'as_num': data[0], 'netblock': data[1],
+    __ip_addr = IPAddress(ip_addr)
+    __network = IPNetwork(data[1])
+    record = {'as_num': int(data[0]), 'network': data[1],
               'as_name': str(app.config['ASNDB'].get_as_name(data[0])),
-              'ip': ip_addr}
+              'ip': ip_addr, 'ip_version': int(__ip_addr.version),
+              'ip_hex': hex(__ip_addr),
+              'network_broadcast': str(__network.broadcast),
+              'network_netmask': str(__network.netmask),
+              'network_hostmask': str(__network.hostmask),
+              'network_size': int(__network.size)}
+    if app.config['DEBUG']:
+        mongo.db.queries.insert(record)
+        _ = record.pop('_id', None)
+    return jsonify(record)
+
+
+@core.route('/network-addresses', methods=['GET'])
+def network_addresses():
+    """Enrich IP address."""
+    cidr = request.args.get('cidr')
+    __network = IPNetwork(cidr)
+    addresses = [str(x) for x in list(__network)]
+    record = {'cidr': cidr, 'network_addresses': addresses,
+              'network_size': int(__network.size)}
+    if app.config['DEBUG']:
+        mongo.db.queries.insert(record)
+        _ = record.pop('_id', None)
+    return jsonify(record)
+
+
+@core.route('/prefixes', methods=['GET'])
+@check_asndb
+def prefixes():
+    """Enrich IP address."""
+    asn = request.args.get('asn')
+    data = app.config['ASNDB'].get_as_prefixes(asn)
+    record = {'as_num': int(asn), 'prefixes': list(data), 'count': len(data)}
+    if app.config['DEBUG']:
+        mongo.db.queries.insert(record)
+        _ = record.pop('_id', None)
+    return jsonify(record)
+
+
+@core.route('/as-name', methods=['GET'])
+@check_asndb
+def as_name():
+    """Enrich IP address."""
+    asn = request.args.get('asn')
+    record = {'as_name': str(app.config['ASNDB'].get_as_name(int(asn))),
+              'as_num': asn}
     if app.config['DEBUG']:
         mongo.db.queries.insert(record)
         _ = record.pop('_id', None)
